23e1386157b56edc11fd30887675cfc9becbc2f7
Farzan Soli <44372399+FarzanSoli@users.noreply.github.com>
2024-08-20 21:56:05 -0300

    updated

--- Code/Sequence_Builder.py
+++ Code/Sequence_Builder.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import itertools
 from Functions import Functions
 from AA_features import features
 # ========================================= #
@@ -11,9 +12,11 @@ H_samples_ordered = torch.load('H_samples_ordered.pt')
 def feature_range(Norm_AAs):
     AA_range = {}
     for key, value in Norm_AAs.items():
+        if key == 'X':
+            continue
         features = []
         for i in range(len(value)):
-            features.append([value[i]*0.97, value[i]*1.03])
+            features.append([value[i]*0.96, value[i]*1.04])
         AA_range[key] = features
     return AA_range
 # ------------------------------------- #
@@ -34,8 +37,18 @@ for i in range(H_samples_ordered.shape[0]):
                                            H_samples_ordered[i,j,0],
                                            H_samples_ordered[i,j,1],
                                            H_samples_ordered[i,j,2]))
+    for i in range(len(Amino_Acids)):
+        if isinstance(Amino_Acids[i], list) and len(Amino_Acids[i]) == 0:
+            Amino_Acids[i] = ['X']
     Proteins.append(Amino_Acids)
 # ------------------------------------- #
+
+# Full_Sequences = list(itertools.product(*Amino_Acids))
+
+
+
+
+
 
 
 
